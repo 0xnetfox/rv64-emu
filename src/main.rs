d8af936f2d0f6e1a276e29461f9312327ae40527
netfox <say-hi@netfox.rip>
2023-02-21 02:03:31 +0100

    add: super-ultra-dupper simple version of a MMU
    To rest a bit from the elf-parser, idea is to implement this
    very basic MMU with 1-byte level perms and start to implement the
    way to load program's static sections into it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+mod mmu;
+
 use elf_parser::parser::ElfParser;
+use crate::mmu::Mmu;
+
+
+struct Emu {
+    memory: Mmu
+}
 
 fn main() {
     let contents = std::fs::read("./out/rv64i-test").unwrap();
     let _ = ElfParser::parse(contents);
+
+    let mmu = Mmu::new(2 * 1024);
 }
---
+++ src/mmu.rs
@@ -0,0 +1,47 @@
+#[allow(dead_code)]
+const PERM_READ:  u8 = 1;
+#[allow(dead_code)]
+const PERM_WRITE: u8 = 2;
+#[allow(dead_code)]
+const PERM_RAW:   u8 = 4;
+#[allow(dead_code)]
+const PERM_EXEC:  u8 = 8;
+
+/// Represents a address on this `Mmu` implementation
+pub struct VirtAddr(usize);
+
+#[derive(Debug, Copy, Clone)]
+pub struct Perm(u8);
+
+#[derive(Debug)]
+pub struct Mmu {
+    pub memory: Vec<u8>,
+    pub permissions: Vec<Perm>
+}
+
+#[allow(dead_code)]
+impl Mmu {
+    pub fn new(size: usize) -> Self {
+        Mmu {
+            memory: vec![0u8; size],
+            permissions: vec![Perm(PERM_RAW); size]
+        }
+    }
+
+    pub fn read_into(&self, buf: &mut [u8], off: VirtAddr) -> Result<(), ()> {
+        // TODO implement perms
+        buf.copy_from_slice(&self.memory[off.0..off.0 + buf.len()]);
+        Ok(())
+    }
+
+    pub fn write_from(&mut self, buf: &[u8], off: VirtAddr) -> Result<(), ()> {
+        if self.permissions[off.0..off.0 + buf.len()]
+            .iter()
+            .all(|b| (b.0 & PERM_WRITE) != 0) {
+            return Err(())
+        }
+
+        self.memory[off.0..off.0 + buf.len()].copy_from_slice(buf);
+        Ok(())
+    }
+}
